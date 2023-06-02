52d6ef86cfd224e7ba5bd8239971f1d64bbcbe7d
Aishat-Akinyemi <aishatadenike01@gmail.com>
2023-06-02 19:29:51 +0100

    create app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import * as React from 'react'
 import { ConnectWallet } from "@thirdweb-dev/react";
 import { Nav } from './components/Nav';
+import {Home} from './components/Home';
 import { SideBar } from './components/SideBar';
+import {Box, Flex, Center  } from '@chakra-ui/react';
 
 export default function App() {
   return (
-    <>
-    <Nav/>
-    <SideBar/>
-    </>
+    <Box>
+      <Nav/>
+      <Home/>
+      <Flex m=''>
+          <SideBar/>
+          <Box flex='1' ml="48px">
+        </Box>
+      </Flex>
+
+    </Box>
   )
 }

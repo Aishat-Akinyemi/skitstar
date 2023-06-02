82cd4cb953e9ad11624ab6570a54244412f2a83e
Riya Agrawal <68336293+Riya500-git@users.noreply.github.com>
2023-06-03 00:33:24 +0530

    Create manage_sub.js

---
+++ src/components/manage_sub.js
@@ -0,0 +1,171 @@
+import React from "react";
+import "./manage_sub.css";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" alt="Logo" />
+          <div className="company-name">SkitStars</div>
+        </div>
+        <div className="navigation">
+          <ul>
+            <li>Our Mission</li>
+            <li>FAQ</li>
+            <li>Become a Creator</li>
+          </ul>
+        </div>
+        <div className="right-section">
+          <i className="bell-icon"></i>
+          <button className="connect-wallet-button">Connect Wallet</button>
+        </div>
+      </header>
+
+      <section>
+        <ul>
+          <h3>Home</h3>
+          <h3>Subscriptions</h3>
+          <h3>Assets</h3>
+          <h3>History</h3>
+          <h3>Settings</h3>
+          <h3>Help</h3>
+        </ul>
+      </section>
+      <div className="search-bar">
+        <input type="text" placeholder="Search Channel" />
+      </div>
+      <section>
+        <div className="header">
+          <div className="profile-section">
+            <img
+              src="profile-image.jpg"
+              alt="Profile"
+              className="profile-image"
+            />
+            <div className="channel-details">
+              <h4>The Good Laugh Zone (TGLZ)</h4>
+              <div className="subscribers">20M Subscribers | 2k posts</div>
+              <div className="wallet-address">
+                Wallet Address: 0xB769...Fe42
+              </div>
+            </div>
+          </div>
+          <button className="subscribe-button">Subscribed</button>
+        </div>
+      </section>
+      <section>
+        <div className="header">
+          <div className="profile-section">
+            <img
+              src="profile-image.jpg"
+              alt="Profile"
+              className="profile-image"
+            />
+            <div className="channel-details">
+              <h4>The Good Laugh Zone (TGLZ)</h4>
+              <div className="subscribers">20M Subscribers | 2k posts</div>
+              <div className="wallet-address">
+                Wallet Address: 0xB769...Fe42
+              </div>
+            </div>
+          </div>
+          <button className="subscribe-button">Subscribed</button>
+        </div>
+      </section>
+      <section>
+        <div className="header">
+          <div className="profile-section">
+            <img
+              src="profile-image.jpg"
+              alt="Profile"
+              className="profile-image"
+            />
+            <div className="channel-details">
+              <h4>The Good Laugh Zone (TGLZ)</h4>
+              <div className="subscribers">20M Subscribers | 2k posts</div>
+              <div className="wallet-address">
+                Wallet Address: 0xB769...Fe42
+              </div>
+            </div>
+          </div>
+          <button className="subscribe-button">Subscribed</button>
+        </div>
+      </section>
+      <section>
+        <div className="header">
+          <div className="profile-section">
+            <img
+              src="profile-image.jpg"
+              alt="Profile"
+              className="profile-image"
+            />
+            <div className="channel-details">
+              <h4>The Good Laugh Zone (TGLZ)</h4>
+              <div className="subscribers">20M Subscribers | 2k posts</div>
+              <div className="wallet-address">
+                Wallet Address: 0xB769...Fe42
+              </div>
+            </div>
+          </div>
+          <button className="subscribe-button">Subscribed</button>
+        </div>
+      </section>
+      <section>
+        <div className="header">
+          <div className="profile-section">
+            <img
+              src="profile-image.jpg"
+              alt="Profile"
+              className="profile-image"
+            />
+            <div className="channel-details">
+              <h4>The Good Laugh Zone (TGLZ)</h4>
+              <div className="subscribers">20M Subscribers | 2k posts</div>
+              <div className="wallet-address">
+                Wallet Address: 0xB769...Fe42
+              </div>
+            </div>
+          </div>
+          <button className="subscribe-button">Subscribed</button>
+        </div>
+      </section>
+      <section>
+        <div className="header">
+          <div className="profile-section">
+            <img
+              src="profile-image.jpg"
+              alt="Profile"
+              className="profile-image"
+            />
+            <div className="channel-details">
+              <h4>The Good Laugh Zone (TGLZ)</h4>
+              <div className="subscribers">20M Subscribers | 2k posts</div>
+              <div className="wallet-address">
+                Wallet Address: 0xB769...Fe42
+              </div>
+            </div>
+          </div>
+          <button className="subscribe-button">Subscribed</button>
+        </div>
+      </section>
+      <footer>
+        <div className="footer-links">
+          <a href="#">About</a>
+          <a href="#">Terms of Service</a>
+          <a href="#">Privacy Policy</a>
+          <a href="#">Contact</a>
+        </div>
+        <div className="social-media">
+          <i className="fab fa-facebook-f"></i>
+          <i className="fab fa-twitter"></i>
+          <i className="fab fa-instagram"></i>
+          <i className="fab fa-linkedin"></i>
+        </div>
+        <p>&copy; 2023 Company Name. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default LandingPage;

b4be2f73bf07adda7772140d8f655f5612466520
Aishat-Akinyemi <aishatadenike01@gmail.com>
2023-06-11 13:36:55 +0100

    update actionbutton element to take other properties

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Button } from '@chakra-ui/react'
 
-export const ActionButton = ({label}) => {
+export const ActionButton = ({label, ...otherProps}) => {
   return (
-    <Button colorScheme='purple' borderRadius="44px" p="16px 32px">
+    <Button colorScheme='purple' borderRadius="44px" p="16px 32px" {...otherProps}>
         {label}
     </Button>
   )

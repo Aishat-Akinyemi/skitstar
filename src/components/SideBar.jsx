e798083ffec21e77f60ddf95aa9d85565867363e
Aishat-Akinyemi <aishatadenike01@gmail.com>
2023-06-11 13:36:12 +0100

    add spacing between sidebar icons and text for each menu item

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,27 +6,27 @@ export const SideBar = () => {
       <Box minWidth="218px" display="flex" flexDirection="column" gap="24px" mr="24px" mt="32px">
           <Menu spacing={6}  color="#626262">
           <MenuItem>
-            <Icon as={Home2} color='brand.900' width="24px" height="24px"/>
+            <Icon as={Home2} color='brand.900' width="24px" height="24px" mr="16px"/>
             Home
           </MenuItem>
           <MenuItem>
-            <Icon as={Profile} color='brand.900' width="24px" height="24px"/>
+            <Icon as={Profile} color='brand.900' width="24px" height="24px" mr="16px"/>
             Profile
           </MenuItem>
           <MenuItem>
-            <Icon size="32px" as={Element4} color='brand.900' width="24px" height="24px"/>
+            <Icon size="32px" as={Element4} color='brand.900' width="24px" height="24px" mr="16px"/>
             Dashboard
           </MenuItem>
           <MenuItem>
-            <Icon as={VideoTime} color='brand.900' width="24px" height="24px"/>
+            <Icon as={VideoTime} color='brand.900' width="24px" height="24px" mr="16px"/>
             History
           </MenuItem>
           <MenuItem>
-            <Icon as={Setting2} color='brand.900' width="24px" height="24px"/>
+            <Icon as={Setting2} color='brand.900' width="24px" height="24px" mr="16px"/>
             Settings
           </MenuItem>
           <MenuItem>
-            <Icon as={MessageQuestion} color='brand.900' width="24px" height="24px"/>
+            <Icon as={MessageQuestion} color='brand.900' width="24px" height="24px" mr="16px"/>
             Help
           </MenuItem>
         </Menu>

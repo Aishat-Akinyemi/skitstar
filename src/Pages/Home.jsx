c1abf8a4b57a53ac958750c4f603f04ee371d284
Aishat-Akinyemi <aishatadenike01@gmail.com>
2023-06-14 10:24:23 +0100

    update home page

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Box, Tag, Input, InputGroup, InputLeftElement, HStack, TagLabel, Icon, Heading  } from '@chakra-ui/react'
+import { SearchNormal } from 'iconsax-react'
+import { VideoListGrid } from '../components/VideoListGrid'
+
+export const Home = () => {
+  return (
+    <Box width="80vw">
+      <HStack spacing={4}>
+          <Tag size="lg" variant='outline'>
+            <TagLabel>All</TagLabel>
+          </Tag>
+          <Tag size="lg" variant='outline'>
+            <TagLabel>Musical</TagLabel>
+          </Tag>
+          <Tag size="lg" variant='outline'>
+            <TagLabel>Satire</TagLabel>
+          </Tag>
+          <Tag size="lg" variant='outline'>
+            <TagLabel>Parody</TagLabel>
+          </Tag>
+          <Tag size="lg" variant='outline'>
+            <TagLabel>Comedy</TagLabel>
+          </Tag>
+          <Tag size="lg" variant='outline'>
+            <TagLabel>Parody</TagLabel>
+          </Tag>
+          <Tag size="lg" variant='outline'>
+            <TagLabel>Sketch</TagLabel>
+          </Tag>
+      </HStack>
+
+      <Box className='search-bar' my="28px">
+        <InputGroup color="">
+          <InputLeftElement pointerEvents='none'>
+            <Icon as={SearchNormal}/>
+          </InputLeftElement>
+          <Input type='search' placeholder='Search'  borderRadius="45px" boxShadow="4px 4px 2px rgba(0, 0, 0, 0.1);"/>
+        </InputGroup>          
+      </Box>
+      <Heading>Trending Videos</Heading>
+      {/* {
+        for (let index = 0; index < 9; index++) {
+          <VideoDisplayCard></VideoDisplayCard>
+        }
+      } */}
+      <VideoListGrid/>
+    </Box>
+  )
+}

b6d343ceb9f128efb51dc9278dd0f04e9203623e
Aishat-Akinyemi <aishatadenike01@gmail.com>
2023-06-15 15:04:16 +0100

    create creator info child components

---
+++ src/components/Ads.jsx
@@ -0,0 +1,68 @@
+import {
+    Box,
+    Card,
+    CardBody,
+    Heading,
+    Stack,
+    Text,
+    Image,
+    Flex,
+  } from "@chakra-ui/react";
+  import React from "react";
+  import { ActionButton } from "./ActionButton";
+  
+  const Ad = () => {
+    const nativetoken = "EVMOS";
+    return (
+      <Flex gap="32px">
+        {/* <Card w="360px" h="552px"> */}
+        <Card maxW="sm">
+          <CardBody>
+            <Image
+              src="https://media.licdn.com/dms/image/C4D03AQEYfSMJT3aPAA/profile-displayphoto-shrink_800_800/0/1656793439753?e=2147483647&v=beta&t=xBxxtaFX6MLF0gXAW4OcOlwhJHvQpbrKHNGp7qs-cMU"
+              borderRadius="lg"
+            />
+            <Stack mt="6" spacing="3">
+              <Heading size="md">Name</Heading>
+              <Flex direction="row" justifyContent="space-between">
+                <Text color="brand.900" fontSize="2xl">
+                  450 {nativetoken}
+                </Text>
+                <Text fontSize="lg">Supply 100</Text>
+              </Flex>
+              {/* <Text>Location</Text> */}
+            </Stack>
+          </CardBody>
+        </Card>
+        <Flex direction="column" justifyContent="space-around" w="692px">
+          <Box>
+            <Heading>Description</Heading>
+            <Text>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+              accusamus quod in neque, quis ullam expedita veritatis rerum, eaque,
+              alias exercitationem omnis fugiat obcaecati facilis pariatur
+              corrupti voluptatum? Fugiat, quis?
+            </Text>
+          </Box>
+          <Heading>Value</Heading>
+          <ActionButton label="Purchase Ads" width="max-content"/>
+        </Flex>
+      </Flex>
+    );
+  };
+
+  const ad = [1,2,3, 4,5]
+  const Ads = () => {
+    return (
+      <Flex direction="column" gap="24px">
+        {
+          ad?.map((ad, id) => (
+            <Ad key={id}/>
+          ))
+        }
+      </Flex>
+    )
+  }
+  
+  export default Ads;
+  

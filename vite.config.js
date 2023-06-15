ab52280de256fe24c7bc5afd32ba58f560c94b65
Aishat-Akinyemi <aishatadenike01@gmail.com>
2023-06-15 15:01:58 +0100

    change node polyfills used in vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,26 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
+// import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
-  define: {
-    "process.env": {},
-  },
+  plugins: [react()],
+  // define: {
+  //   "process.env": {},
+  // },
+  optimizeDeps: {
+    esbuildOptions: {
+        define: {
+            global: 'globalThis',
+            // "process.env": {},
+        },
+        plugins: [
+            NodeGlobalsPolyfillPlugin({
+                buffer: true,
+                process: true
+            })
+        ]
+    }
+}
 });

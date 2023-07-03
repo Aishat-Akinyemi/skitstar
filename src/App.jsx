ad1c1664449f4c53369a92b47cec4a0e65b5c5e4
Aishat-Akinyemi <aishatadenike01@gmail.com>
2023-07-03 21:27:36 +0100

    uodate routing

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,35 @@ import { Footer } from './components/Footer';
 import { CreatorRegistration } from './Pages/CreatorRegistration';
 import { VideoUploadForm } from './Pages/VideoUploadForm';
 import { EventForm } from './Pages/EventForm';
+import { AdsVoucherForm } from './Pages/AdsVoucherForm';
+import { MintNftForm } from './Pages/MintNftForm';
+import { Profile } from './Pages/Profile';
 import { BuyAdsVoucherForm } from './Pages/BuyAdsVoucherForm';
 import { AboutCreator } from './Pages/AboutCreator';
 import { VideoPlayer } from './components/VideoPlayer';
-import { Profile } from './Pages/Profile';
-import { useConnectionStatus, useSigner, useContract } from "@thirdweb-dev/react";
+
+import { useConnectionStatus, useAddress, useContract, useContractRead } from "@thirdweb-dev/react";
 import { Routes, Route } from "react-router-dom";
 import { useToast } from '@chakra-ui/react';
+// import { useNavigate } from 'react-router-dom';
+import { isZeroAddress } from './utils/utils';
 
 export default function App() {
-  const connectionStatus = useConnectionStatus();  
+  const connectionStatus = useConnectionStatus(); 
+  // const navigate = useNavigate();
+  const address = useAddress();
+  // const storage = useStorage(); 
   const { contract: skitStarContract } = useContract(import.meta.env.VITE_SKITSTAR_ADD);
-  const toast = useToast();
-  const [currentAddress, setcurrentAddress] = useState();
-  const [isCreator, setIsCreator] = useState(false);
+  const { data: creatordata, isLoading: isLoadingCreatordata, error: creatordataError } = useContractRead(skitStarContract, "getStar", [address]);
+  const { contract: marketPlaceContract} = useContract(import.meta.env.VITE_MKTPLACE_ADD);
+  const { data: allCreators } = useContractRead(skitStarContract, "getAllCreators");
 
+  const toast = useToast();
+  const [isCreator, setIsCreator] = useState(false);
+  const tempCreator  =     ["0xeB555a1D554F14c1d052460065f020952614FE72", "0xeB555a1D554F14c1d052460065f020952614FE72"]
+  // useEffect(() => {
+  //   console.log(allCreators)
+  // }, [allCreators])
   
 
   function toaster(message, status) {
@@ -37,17 +51,22 @@ export default function App() {
   
   return (
       <Box mx="24px">
-        <Nav isCreator={isCreator}/>        
+        <Nav isCreator={creatordata && !isZeroAddress(creatordata.ERC1155TokenAddress) }/>        
             {
               connectionStatus == "connected" ?
               <Flex minHeight="90vh">
                  <SideBar/>
                   <Box mt="40px" mb="212px">
                     <Routes>
-                      <Route path="/" element={<Home />} />
+                      <Route path="/" element={<Home creatorList={tempCreator}/>} />
                       <Route path='/creator/join' element={<CreatorRegistration contract={skitStarContract} toaster={toaster} setIsCreator={setIsCreator}/>}/>
+                      <Route path='/creator' element={<AboutCreator contract={skitStarContract} />}/>
                       <Route path="/profile" element={<Profile contract={skitStarContract}/>} />
                       <Route path='/creator/upload' element={<VideoUploadForm  toaster={toaster}  contract={skitStarContract}/>}/>
+                      <Route path='/creator/mint-nft' element={<MintNftForm  toaster={toaster} erc1155ContractAdd={creatordata ? creatordata.ERC1155TokenAddress :null} marketPlaceContract={marketPlaceContract}/>}/>
+                      <Route path='/creator/mint-event-tickets' element={<EventForm  toaster={toaster} erc1155ContractAdd={creatordata ? creatordata.ERC1155TokenAddress :null} marketPlaceContract={marketPlaceContract}/>}/>
+                      <Route path='/creator/mint-ads-voucher' element={<AdsVoucherForm toaster={toaster} erc1155ContractAdd={creatordata ? creatordata.ERC1155TokenAddress :null} marketPlaceContract={marketPlaceContract}/>}/>
+                        
                     </Routes>
                   </Box>
              </Flex>

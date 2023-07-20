348e3e2575f5be0d714a2ba9dcf1728e939c0bb6
Aishat-Akinyemi <aishatadenike01@gmail.com>
2023-07-20 11:45:51 +0100

    create util class for misc functions

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,38 @@
 import { ethers } from "ethers";
 
 export const isZeroAddress = (addressToCheck) => ethers.constants.AddressZero === addressToCheck;
+
+export function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  export function classifyArrayType(tokensArray) {
+    const nftArray = [];
+    const eventArray = [];
+    const adArray = [];
+
+    tokensArray.forEach((item) => {
+      switch (item.metadata.type) {
+        case "nft":
+          nftArray.push(item);
+          break;
+        case "event":
+          eventArray.push(item);
+          break;
+        case "ads":
+          adArray.push(item);
+          break;
+        default:
+          break;
+      }
+    });
+    return {
+      nfts: nftArray,
+      events: eventArray,
+      ads: adArray
+    }
+  }

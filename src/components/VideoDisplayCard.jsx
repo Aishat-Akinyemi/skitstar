f53cef8fc0841977d80c5a88091bf8ac21f05bc9
Aishat-Akinyemi <aishatadenike01@gmail.com>
2023-08-03 00:47:48 +0100

    update videodisplay card to properly display dynamic videos

--- src/components/VideoDisplayCard.jsx
+++ src/components/VideoDisplayCard.jsx
@@ -1,44 +1,65 @@
 import { Box, Card, CardBody, CardFooter, HStack, Text, Image, VStack, Avatar } from '@chakra-ui/react'
 import React from 'react'
 import PressPlayBro from "../assets/PressPlayBro.svg"
-
-
-export const VideoDisplayCard = ({src, title, creatorAvatar, creatorName, views, datePosted}) => {
+import { VideoPlayer } from './VideoPlayer'
+import { useNavigate } from 'react-router-dom'
+export const VideoDisplayCard = ({video, isLoading}) => {
     let temp = {thumbnail :"https://www.techsmith.com/blog/wp-content/uploads/2019/06/YouTube-Thumbnail-Sizes.png", 
-    title:"TGIF Comedy series | A special Friday show ", creatorAvatar: "https://media.licdn.com/dms/image/C4D03AQEYfSMJT3aPAA/profile-displayphoto-shrink_800_800/0/1656793439753?e=2147483647&v=beta&t=xBxxtaFX6MLF0gXAW4OcOlwhJHvQpbrKHNGp7qs-cMU", 
-    creatorName:"The Good Laugh Zone", views:"20M", datePosted:"2/2/2023"}
+   }
+   
+    const navigate = useNavigate()
+    const displayVideo=()=> { 
+        navigate(`/play/${video.creatorAddress}/${video.playbackId}`, { state: video });
+    }
+
+    const goToCreatorPage=() => {
+        console.log(video)
+        navigate(`/creator/${video.creatorAddress}`, {state: {creatorAddress: video.creatorAddress, 
+                                                      creatorERC1155TokenAddress: video.creatorERC1155TokenAddress
+                                                    }});
+    }
+
+    if (video == null){
+        return <></>
+    } else if (isLoading){
+        //TODO implement isLoading in parent elements/references
+        return <>Loading...</>
+    }
     return (
     <Box display="flex" 
          flexDirection="column" 
          alignItems="flex-start" 
          gap="16px"
          w="360px"
-         h="340px"
+         h="340px"        
     >
         <Image 
                 src={temp.thumbnail} 
                 borderRadius="10px"
                 w="360px"
                 h="232px" 
+                onClick={displayVideo}
 
         />
-        <HStack gap="16px">
-            <Avatar size="md" name={temp.creatorName} src={temp.creatorAvatar}></Avatar>
+        <HStack gap="16px"
+             onClick={goToCreatorPage}
+        >
+            <Avatar size="md" name={video.creatorName} src={video.creatorAvatar}></Avatar>
             <VStack gap="16px" alignItems="flex-start">
                 <Text fontFamily="Open Sans" fontWeight="700" fontSize="16px" lineHeight="22px"
                     fontStyle="normal" h="24px" w="250px"
                 >
-                    {temp.title}
+                    {video.name}
                 </Text>
                 <Text fontFamily="Open Sans" fontWeight="600" fontSize="12px" lineHeight="16px"
                     fontStyle="normal" color="#828282" mt="8px" 
                 >
-                    {temp.creatorName}
+                    {video.creatorName}
                 </Text>
                 <Text fontFamily="Open Sans" fontWeight="600" fontSize="12px" lineHeight="16px"
                     fontStyle="normal" color="#828282" textAlign="left"
                 >
-                    {temp.views} views | Posted {new Date(temp.datePosted).toDateString()}
+                    {video.viewCount} views | Posted {new Date(video.date).toDateString()}
                 </Text>
             </VStack>
         </HStack>

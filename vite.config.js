3d140b7602eee1102ad7b0f79fd71c0df9ef05ca
Aishat-Akinyemi <aishatadenike01@gmail.com>
2023-08-20 18:16:27 +0100

    revert all changes made to vite.config from initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,81 @@
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// // import { nodePolyfills } from "vite-plugin-node-polyfills";
+// import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+// import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
+// import nodePolyfills from "rollup-plugin-polyfill-node"
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // define: {
+//   //   "process.env": {},
+//   // },
+//   optimizeDeps: {
+//     esbuildOptions: {
+//         define: {
+//             global: 'globalThis',
+//             // "process.env": {},
+//         },
+//         plugins: [
+//             NodeGlobalsPolyfillPlugin({
+//                 buffer: true,
+//                 process: true, 
+            
+//             }),
+//             NodeModulesPolyfillPlugin()
+//         ]
+//     }
+// },
+// build: {
+//     rollupOptions: {
+//         plugins: [
+//             // Enable rollup polyfills plugin
+//             // used during production bundling
+//             nodePolyfills()
+//         ]
+//     }
+// }
+// });
+
+
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// // import { nodePolyfills } from "vite-plugin-node-polyfills";
+// import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // define: {
+//   //   "process.env": {},
+//   // },
+//   optimizeDeps: {
+//     esbuildOptions: {
+//         define: {
+//             global: 'globalThis',
+//             // "process.env": {},
+//         },
+//         plugins: [
+//             NodeGlobalsPolyfillPlugin({
+//                 buffer: true,
+//                 process: true
+//             })
+//         ]
+//     }
+// }
+// });
+
+
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-// import { nodePolyfills } from "vite-plugin-node-polyfills";
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // define: {
-  //   "process.env": {},
-  // },
-  optimizeDeps: {
-    esbuildOptions: {
-        define: {
-            global: 'globalThis',
-            // "process.env": {},
-        },
-        plugins: [
-            NodeGlobalsPolyfillPlugin({
-                buffer: true,
-                process: true, 
-            
-            }),
-            NodeModulesPolyfillPlugin()
-        ]
-    }
-},
-build: {
-    rollupOptions: {
-        plugins: [
-            // Enable rollup polyfills plugin
-            // used during production bundling
-            rollupNodePolyFill()
-        ]
-    }
-}
+  plugins: [react(), nodePolyfills()],
+  define: {
+    "process.env": {},
+  },
 });

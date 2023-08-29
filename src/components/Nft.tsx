6cccd475d93618da06889879364c5eb898f0cbd6
Aishat-Akinyemi <aishatadenike01@gmail.com>
2023-08-29 18:21:07 +0100

    remove commented out code

--- src/components/Nft.tsx
+++ src/components/Nft.tsx
@@ -172,3 +172,163 @@ let dd = {
     },
   ],
 };
+
+
+// [
+//   {
+//       "assetContractAddress": "0xB249e611A3D61C9A77de4be690864EB6Efe80147",
+//       "currencyContractAddress": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+//       "pricePerToken": "1000000000000000000",
+//       "currencyValuePerToken": {
+//           "name": "Fantom",
+//           "symbol": "FTM",
+//           "decimals": 18,
+//           "value": {
+//               "type": "BigNumber",
+//               "hex": "0x0de0b6b3a7640000"
+//           },
+//           "displayValue": "1.0"
+//       },
+//       "id": "3",
+//       "tokenId": "0",
+//       "quantity": "9",
+//       "startTimeInSeconds": 1688401603,
+//       "asset": {
+//           "name": "Comedy Central Town",
+//           "description": "This  is an awesome comedy night, you don't wanna miss it!",
+//           "image": "https://9a4ec705b9124c203c333e01b7156fc0.ipfscdn.io/ipfs/bafybeibgv5bsrkmjbk2p2kcydcdq3r77aexlsygzxqx6anp6jcn5jysdeu/",
+//           "id": "0",
+//           "uri": "ipfs://QmSDDaKi5E6wfE3dvct71vPAVgvY8CAkmcG2KyH84FDF3w",
+//           "location": "Island Club",
+//           "date": "2023-07-21T21:24",
+//           "type": "event"
+//       },
+//       "endTimeInSeconds": 2003759407,
+//       "creatorAddress": "0xeB555a1D554F14c1d052460065f020952614FE72",
+//       "isReservedListing": false,
+//       "status": 4
+//   },
+//   {
+//       "assetContractAddress": "0xB249e611A3D61C9A77de4be690864EB6Efe80147",
+//       "currencyContractAddress": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+//       "pricePerToken": "1000000000000000000",
+//       "currencyValuePerToken": {
+//           "name": "Fantom",
+//           "symbol": "FTM",
+//           "decimals": 18,
+//           "value": {
+//               "type": "BigNumber",
+//               "hex": "0x0de0b6b3a7640000"
+//           },
+//           "displayValue": "1.0"
+//       },
+//       "id": "4",
+//       "tokenId": "1",
+//       "quantity": "10",
+//       "startTimeInSeconds": 1688402558,
+//       "asset": {
+//           "name": "Genesis_One",
+//           "description": "The Genesis NFTs!  Buy to gain access to my Token gated videos!",
+//           "id": "1",
+//           "uri": "ipfs://Qmb2GS8H5RA51mztLZcYEpfStgFDtrqz9vbhhLUGS6tgK3",
+//           "type": "nft"
+//       },
+//       "endTimeInSeconds": 2003762393,
+//       "creatorAddress": "0xeB555a1D554F14c1d052460065f020952614FE72",
+//       "isReservedListing": false,
+//       "status": 4
+//   },
+//   {
+//       "assetContractAddress": "0xB249e611A3D61C9A77de4be690864EB6Efe80147",
+//       "currencyContractAddress": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+//       "pricePerToken": "10000000000000000000",
+//       "currencyValuePerToken": {
+//           "name": "Fantom",
+//           "symbol": "FTM",
+//           "decimals": 18,
+//           "value": {
+//               "type": "BigNumber",
+//               "hex": "0x8ac7230489e80000"
+//           },
+//           "displayValue": "10.0"
+//       },
+//       "id": "5",
+//       "tokenId": "2",
+//       "quantity": "10",
+//       "startTimeInSeconds": 1688403290,
+//       "asset": {
+//           "name": "1 mins ads ",
+//           "description": "A minute Ad in our Video for our countless viewers. Your ads are remain in  our video for ever.",
+//           "id": "2",
+//           "uri": "ipfs://QmPdAcz11NXRMkd3VdFaLyEUAYK3ZGhtM9ajeosUofFkzy",
+//           "type": "ads"
+//       },
+//       "endTimeInSeconds": 2003762706,
+//       "creatorAddress": "0xeB555a1D554F14c1d052460065f020952614FE72",
+//       "isReservedListing": false,
+//       "status": 4
+//   },
+//   {
+//       "assetContractAddress": "0xB249e611A3D61C9A77de4be690864EB6Efe80147",
+//       "currencyContractAddress": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+//       "pricePerToken": "20000000000000000000",
+//       "currencyValuePerToken": {
+//           "name": "Fantom",
+//           "symbol": "FTM",
+//           "decimals": 18,
+//           "value": {
+//               "type": "BigNumber",
+//               "hex": "0x01158e460913d00000"
+//           },
+//           "displayValue": "20.0"
+//       },
+//       "id": "6",
+//       "tokenId": "3",
+//       "quantity": "9",
+//       "startTimeInSeconds": 1688403656,
+//       "asset": {
+//           "name": "2 mins ads ",
+//           "description": "2 minutes Ad in our Video for our countless viewers. Your ads are remain in  our video for ever.",
+//           "id": "3",
+//           "uri": "ipfs://QmVSCgQURKujcp1JfSHJNQRE5kbk4kyvGgG2kEECaoscni",
+//           "type": "ads"
+//       },
+//       "endTimeInSeconds": 2003762700,
+//       "creatorAddress": "0xeB555a1D554F14c1d052460065f020952614FE72",
+//       "isReservedListing": false,
+//       "status": 4
+//   },
+//   {
+//       "assetContractAddress": "0xB249e611A3D61C9A77de4be690864EB6Efe80147",
+//       "currencyContractAddress": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+//       "pricePerToken": "0",
+//       "currencyValuePerToken": {
+//           "name": "Fantom",
+//           "symbol": "FTM",
+//           "decimals": 18,
+//           "value": {
+//               "type": "BigNumber",
+//               "hex": "0x00"
+//           },
+//           "displayValue": "0.0"
+//       },
+//       "id": "10",
+//       "tokenId": "4",
+//       "quantity": "14",
+//       "startTimeInSeconds": 1689934370,
+//       "asset": {
+//           "name": "Comedy extraveganza",
+//           "description": "A night to remember, featuring Top comedians",
+//           "image": "https://9a4ec705b9124c203c333e01b7156fc0.ipfscdn.io/ipfs/bafybeihmccbeycyyuq5mismxpo3ccrtdfa5ojvh5rkwhx5lzf4jccxjj4y/",
+//           "id": "4",
+//           "uri": "ipfs://QmXUfG4GTJreG6ZqvQTFx5jm4XSYTMi8x31uyYSzvo3PT2",
+//           "location": "Theatre on Siddle",
+//           "date": "2023-07-23T11:10",
+//           "type": "event"
+//       },
+//       "endTimeInSeconds": 2005294230,
+//       "creatorAddress": "0xeB555a1D554F14c1d052460065f020952614FE72",
+//       "isReservedListing": false,
+//       "status": 4
+//   }
+// ]
